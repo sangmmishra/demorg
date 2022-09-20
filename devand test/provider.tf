0c3fda41567d55c5f88647b860a40bdb10efe06c
sangam <110962434+sangmmishra@users.noreply.github.com>
2022-09-20 07:21:40 +0000

    Update provider.tf

--- devand test/provider.tf
+++ devand test/provider.tf
@@ -3,3 +3,12 @@ provider "azurerm" {
 
    version        = "=3.0.0"
  }
+
+terraform {
+  backend "azurerm" {
+    storage_account_name = "sangamrg8ce4"
+    container_name       = "container3"
+    key                  = "prod.terraform.tfstate"
+    access_key = "z+pI07poI/4mz5xrYdNue3RWya2bRMcdD3S/e5o8C8GLAMrFgwp2CwMTNKZVY8DqiLUBIHBwsz3X+ASt6fVG5Q=="
+  }
+}
